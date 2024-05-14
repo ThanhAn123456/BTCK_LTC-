84b6f083682e5447a19216c9e0fe4cb61a15cbcf
21115053120201 <21115053120201@sv.ute.udn.vn>
2024-05-15 01:23:01 +0700

    create validation in employee

--- BTCK_LTC#/Program.cs
+++ BTCK_LTC#/Program.cs
@@ -1,5 +1,7 @@
 using BTCK_LTC_.Models;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 namespace BTCK_LTC_
 {
@@ -14,6 +16,16 @@ namespace BTCK_LTC_
             builder.Services.AddDbContext<QuanLyBaiDangCongTyContext>(options => options.UseSqlServer(
                 builder.Configuration.GetConnectionString("QuanLyBaiDangCongTyConnection")));
             var app = builder.Build();
+
+            var supportedCultures = new[] { new CultureInfo("en-GB") }; // dd/MM/yyyy
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-GB"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            };
+
+            app.UseRequestLocalization(localizationOptions);
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
